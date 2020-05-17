20f0d4513a5f6e807083ccc8c0e2aaa71d4c276a
Nishank Das <animus@Nishanks-MacBook-Pro.local>
2020-05-17 09:18:22 +0530

    LeetCode PostorderStackImpl.java

---
+++ Tree/PostOrderTraversalStackImpl.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root == null){
+            return new ArrayList<>();
+        }
+        LinkedList<Integer> results = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(stack.size() != 0){
+            TreeNode curr = stack.pop();
+            results.addFirst(curr.val);
+            if(curr.left != null){
+                stack.push(curr.left);
+            }
+            if(curr.right != null){
+                stack.push(curr.right);
+            }
+        }
+        return results;
+    }
+}
